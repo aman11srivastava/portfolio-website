0831decebedc249c98ea87ea24ee1237c7e4f6c0
aman11srivastava <aman11srivastava@gmail.com>
2020-12-30 18:07:58 +0530

    Added hover effect on social media icons, added a read-more option in the projects section with a expandMore Icon

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { projectDetails } from '../../constants/Project'
 import './Projects.css'
 
@@ -12,6 +12,8 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import VisibilityIcon from '@material-ui/icons/Visibility';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 const useStyles = makeStyles({
     root: {
@@ -27,39 +29,71 @@ const useStyles = makeStyles({
 //   rgb(227, 238, 255)
 
 const Projects = () => {
+
+    const [ heightOfSection, setHeightOfSection ] = useState("")
+    const [ isExpanded, setExpanded ] = useState(false)
+
+    const handleDivSize = (e) => {
+        e.stopPropagation()
+        e.preventDefault()
+        setHeightOfSection('650px')
+    }
+    
+    const handleIncrementOfDiv = (e) => {
+        e.stopPropagation();
+        e.preventDefault()
+        setHeightOfSection('auto')
+    }
+
     const classes = useStyles();
     return (
         <div style = {{paddingTop: '20px', background: '  rgb(227, 238, 255)', marginTop: '-10px', paddingBottom: '70px'}}>
             <h1 className = "heading">Projects</h1>
-        <div className = "grid-container">
-            {projectDetails.map((value) => (
-            <Card className={classes.root}>
-            <CardActionArea>
-            <CardMedia
-                className={classes.media}
-                image={value.image}
-                title={value.name}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    {value.name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    {value.description} ({value.date})
-                </Typography>
-            </CardContent>
-            </CardActionArea>
-            <CardActions>
-            <Button size="small" target = "_blank" href = {value.gitHub} color="primary">
-                View on GitHub
-            </Button>
-            <Button size="small" target = "_blank" href = {value.url} color="primary">
-                View Live Project
-            </Button>
-            </CardActions>
-        </Card>
-        ))}
-      </div>
+            <div className = "read-more-section" onClick = {handleDivSize} style = {{height: heightOfSection}}>
+                <div className = "grid-container">
+                    {projectDetails.map((value) => (
+                    <Card className={classes.root}>
+                    <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image={value.image}
+                        title={value.name}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {value.name}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {value.description} ({value.date})
+                        </Typography>
+                    </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                    <Button size="small" target = "_blank" href = {value.gitHub} color="primary">
+                        View on GitHub
+                    </Button>
+                    <Button size="small" target = "_blank" href = {value.url} color="primary">
+                        View Live Project
+                    </Button>
+                    </CardActions>
+                </Card>
+                ))}
+            </div>
+          </div>
+        
+          <a 
+            href = "#" 
+            id = "read-more" 
+            onClick = {handleIncrementOfDiv} 
+            style = {{height: heightOfSection}}
+            >
+                <ExpandMoreIcon className = "icon" />
+                {/* {isExpanded ? 
+                    <ExpandLessIcon/>
+                    : 
+                    <ExpandMoreIcon/>
+                } */}
+            </a>
       </div>
     )
 }
